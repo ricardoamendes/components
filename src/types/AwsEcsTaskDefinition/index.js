d72ecb9da7030104b3e4f88f82290e9327bf7ad5
Raees Iqbal <raees.bhatti@gmail.com>
2018-10-09 20:30:03 +0500

    Task Defintiion updates

--- src/types/AwsEcsTaskDefinition/index.js
+++ src/types/AwsEcsTaskDefinition/index.js
@@ -1,53 +1,85 @@
-const aws = require('aws-sdk')
-const ecs = new aws.ECS({ region: process.env.AWS_DEFAULT_REGION || 'us-east-1' })
+import { pick } from 'ramda'
 
-const deploy = async (inputs, context) => {
-  const { state } = context
+const inputsProps = [
+  'provider',
+  'family',
+  'cpu',
+  'memory',
+  'networkMode',
+  'placementConstraints',
+  'requiresCompatibilities',
+  'executionRoleArn',
+  'taskRoleArn',
+  'containerDefinitions',
+  'volumes'
+]
 
-  if (state.family && inputs.family !== state.family) {
-    context.log('Change to ECS TaskDefinition "family" requires replacement. Making one now...')
-    await remove({}, context)
+export default {
+  async deploy(prevInstance, context) {
+    const inputs = pick(inputsProps, this)
+    const state = context.getState(this)
+    const AWS = new this.provider.getSdk()
+    const ecs = AWS.ECS()
+
+    if (state.family && inputs.family !== state.family) {
+      context.log('Change to ECS TaskDefinition "family" requires replacement. Making one now...')
+      await this.remove({}, context)
+    }
+
+    const { taskDefinition } = await ecs.registerTaskDefinition(inputs).promise()
+    context.log(`ECS TaskDefinition registered: "${inputs.family}"`)
+
+    // Remove previous revision
+    if (state.hasOwnProperty('family') && state.hasOwnProperty('revision')) {
+      await this.remove({}, context)
+    }
+
+    context.saveState(this, taskDefinition || {})
+    return Object.assign(this, taskDefinition)
+  },
+
+  async remove(prevInstance, context) {
+    const AWS = new this.provider.getSdk()
+    const ecs = AWS.ECS()
+    const state = context.getState(this)
+    if (!state.hasOwnProperty('family') || !state.hasOwnProperty('revision')) return {}
+
+    await ecs
+      .deregisterTaskDefinition({ taskDefinition: `${state.family}:${state.revision}` })
+      .promise()
+    context.log(`ECS TaskDefinition revision deregistered: "${state.family}:${state.revision}"`)
+
+    context.saveState(this, {})
+    return Object.assign(this, {
+      taskDefinitionArn: null,
+      containerDescriptions: null,
+      family: null,
+      taskRoleArn: null,
+      executionRoleArn: null,
+      networkMode: null,
+      revisions: null,
+      volumes: null,
+      status: null,
+      requiresAttributes: null,
+      placementConstraints: null,
+      compatibilities: null,
+      requiresCompatibilities: null,
+      cpu: null,
+      memory: null
+    })
+  },
+
+  async get(prevInstance, context) {
+    const AWS = new this.provider.getSdk()
+    const ecs = AWS.ECS()
+    const state = context.getState(this)
+    if (!state.hasOwnProperty('family') || !state.hasOwnProperty('revision')) return {}
+
+    const { taskDefinition } = await ecs
+      .describeTaskDefinition({ taskDefinition: `${state.family}:${state.revision}` })
+      .promise()
+
+    context.saveState(this, taskDefinition || {})
+    return Object.assign(this, taskDefinition)
   }
-
-  const { taskDefinition } = await ecs.registerTaskDefinition(inputs).promise()
-  context.log(`ECS TaskDefinition registered: "${inputs.family}"`)
-
-  // Remove previous revision
-  if (state.hasOwnProperty('family') && state.hasOwnProperty('revision')) {
-    await remove({}, context)
-  }
-
-  context.saveState(taskDefinition || {})
-  return taskDefinition
-}
-
-const remove = async (inputs, context) => {
-  const { state } = context
-  if (!state.hasOwnProperty('family') || !state.hasOwnProperty('revision')) return {}
-
-  await ecs
-    .deregisterTaskDefinition({ taskDefinition: `${state.family}:${state.revision}` })
-    .promise()
-  context.log(`ECS TaskDefinition revision deregistered: "${state.family}:${state.revision}"`)
-
-  context.saveState({})
-  return {}
-}
-
-const get = async (inputs, context) => {
-  const { state } = context
-  if (!state.hasOwnProperty('family') || !state.hasOwnProperty('revision')) return {}
-
-  const { taskDefinition } = await ecs
-    .describeTaskDefinition({ taskDefinition: `${state.family}:${state.revision}` })
-    .promise()
-
-  context.saveState(taskDefinition || {})
-  return taskDefinition
-}
-
-module.exports = {
-  deploy,
-  get,
-  remove
 }
